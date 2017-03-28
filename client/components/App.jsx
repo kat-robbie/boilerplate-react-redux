504087b587ce65b9a1abecc34a2d0b90c3fbc6d3
Pouramua <poukupenga@gmail.com>
2017-03-29 09:47:00 +1300

    Adding a Winners component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import AddWord from '../containers/AddWord'
 import Words from '../containers/Words'
+import Winners from '../containers/Winners'
 
 function App (props) {
   return (
     <div className='app-container'>
       <Words />
       <AddWord />
+      <Winners />
     </div>
   )
 }
---
+++ client/containers/Winners.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Winners () {
+  return (
+      <div className='winner-section'>
+        <h2>Winners</h2>
+        <table>
+          <tr>
+            <td>Mix</td>
+            <td>12</td>
+            <td><button>+</button></td>
+            <td><button>-</button></td>
+          </tr>
+          <tr>
+            <td>Don</td>
+            <td>11</td>
+            <td><button>+</button></td>
+            <td><button>-</button></td>
+          </tr>
+        </table>
+      </div>
+  )
+}
+
+export default Winners
