c9e89e535df9695354fa1770fb5bb3a61d3abed0
todddrinkwater <todddrinkwaternz@gmail.com>
2017-03-29 09:40:47 +1300

    working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,6 +8,23 @@ function App (props) {
     <div className='app-container'>
       <Words />
       <AddWord />
+      <div className="winnersSection">
+        <h2>Winners</h2>
+          <table class="table">
+            <tr>
+              <td>Mix</td>
+              <td>12</td>
+              <td><button>+</button></td>
+              <td><button>-</button></td>
+            </tr>
+            <tr>
+              <td>Don</td>
+              <td>11</td>
+              <td><button>+</button></td>
+              <td><button>-</button></td>
+            </tr>
+          </table>
+      </div>
     </div>
   )
 }
