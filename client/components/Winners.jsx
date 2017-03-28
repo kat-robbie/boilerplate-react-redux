f915aa2da3075cda47bf706564252daca07ba8bc
AidanNairn <aidan.nairn95@gmail.com>
2017-03-29 10:05:46 +1300

    Create winnersData array consisting of winner objects.

--- client/components/Winners.jsx
+++ client/components/Winners.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 
 function Winners () {
 
+  const winnersData = [
+    {id: 1, name: 'Aidan', score: 21},
+    {id: 2, name: 'Mix', score: 12},
+    {id: 3, name: 'Don', score: 11}
+  ]
+
   return (
     <div className='winners-section'>
       <h2>Winners</h2>
       <table>
         <tbody>
-          <tr>
-            <td>Mix</td>
-            <td>12</td>
-            <td><button>+</button></td>
-            <td><button>-</button></td>
-          </tr>
-          <tr>
-            <td>Don</td>
-            <td>11</td>
-            <td><button>+</button></td>
-            <td><button>-</button></td>
-          </tr>
+          {winnersData.map(winnerRec => {
+            return (
+              <tr key={winnerRec.id}>
+                <td>{winnerRec.name}</td>
+                <td>{winnerRec.score}</td>
+                <td><button>+</button></td>
+                <td><button>-</button></td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
