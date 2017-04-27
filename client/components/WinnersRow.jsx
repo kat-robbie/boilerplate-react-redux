3a018d93e82c8a2cb64c04b7b73d77115a0327b1
Kat Robbie <katherine.robbie@gmail.com>
2017-04-27 20:39:10 +1200

    test

---
+++ client/components/WinnersRow.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function WinnerRow (props) {
+  return (
+    <tr >
+      <td>{props.name}</td>
+      <td>{props.score}</td>
+      <td><button>+</button></td>
+      <td><button>-</button></td>
+    </tr>
+
+  )
+}
+
+export default WinnerRow
